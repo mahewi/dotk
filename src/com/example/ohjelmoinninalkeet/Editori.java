838fd26d25135f7dcf32d6056d7e760305315675
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2014-03-09 23:59:27 +0200

    Testailtu koodin viemistä pythonille Editori-luokassa

--- src/com/example/ohjelmoinninalkeet/Editori.java
+++ src/com/example/ohjelmoinninalkeet/Editori.java
@@ -1,5 +1,6 @@
 package com.example.ohjelmoinninalkeet;
 
+//import com.sun.nio.sctp.Notification;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.ExternalResource;
@@ -7,26 +8,67 @@ import com.vaadin.ui.Link;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.TextArea;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Label;
+
+
+import java.io.*;
 
 public class Editori extends Panel implements View {
 
 	public static final String NAME = "editoriView";
+	
+	// Komponentit
+	
+	VerticalLayout vlay = new VerticalLayout();
+	TextArea tekstikentta = new TextArea("Koodia tähän");
+	Button nappula = new Button("Arvioi");
+	Label label = new Label();
+	Link lnk = new Link("BÄK!:D", new ExternalResource("#!" + PythonUI.NAME));
 	
 	public Editori() {
 		initLayout();
 	}
 	
 	public void initLayout() {
-		Link lnk = new Link("BÄK!:D", new ExternalResource("#!" + PythonUI.NAME));
-		VerticalLayout vlay = new VerticalLayout();
-		vlay.addComponent(lnk);
-		TextArea tekstikentta = new TextArea("Koodia tähän");
+		
 		tekstikentta.setWidth(50.0f, TextArea.Unit.PERCENTAGE);
 		tekstikentta.setRows(30);
+		
+		vlay.addComponent(lnk);
 		vlay.addComponent(tekstikentta);
+		vlay.addComponent(nappula);
+		vlay.addComponent(label);
+		
+		// Tapahtuman käsittely nappulalle
+		
+		nappula.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				label.setValue(toPython(tekstikentta.getValue()));
+			}
+		});
+		
 		setContent(vlay);
 	}
+	
+	public String toPython(String code) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("test.py"));
+			out.write(code);
+			out.close();
 
+			ProcessBuilder pb = new ProcessBuilder("python", "test.py");
+			Process p = pb.start();
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String paluu = new String(in.readLine());
+			return paluu;
+		}
+		catch(Exception e){return e.toString();}
+	}
+	
 	@Override
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
